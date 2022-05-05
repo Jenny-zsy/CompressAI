fe582c1416dc77314aad28dcec2a7d5bbdc85bec
Jossieee <1337139691@qq.com>
2022-05-05 16:21:12 +0800

    first_verion

--- baseline/dataset.py
+++ baseline/dataset.py
@@ -1,81 +1,128 @@
-# Copyright (c) 2021-2022, InterDigital Communications, Inc
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted (subject to the limitations in the disclaimer
-# below) provided that the following conditions are met:
-
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name of InterDigital Communications, Inc nor the names of its
-#   contributors may be used to endorse or promote products derived from this
-#   software without specific prior written permission.
-
-# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
-# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
-# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import os
+import h5py
+import glob
+import random
+import numpy as np
+import torch
 
 from pathlib import Path
-
+from torchvision import transforms
 from PIL import Image
 from torch.utils.data import Dataset
 
+def Im2Patch(img, win, stride=1):
+    """
+    crop images
 
-class ImageFolder(Dataset):
-    """Load an image folder database. Training and testing image samples
-    are respectively stored in separate directories:
+    return: imgs with shape(channel, patch_size, patch_size, num)
+    """
+    k = 0
+    endc = img.shape[0]
+    endw = img.shape[1]
+    endh = img.shape[2]
+    patch = img[:, 0:endw - win + 0 + 1:stride, 0:endh - win + 0 + 1:stride]
+    TotalPatNum = patch.shape[1] * patch.shape[2]
+    Y = np.zeros([endc, win * win, TotalPatNum], np.float32)
+    for i in range(win):
+        for j in range(win):
+            patch = img[:, i:endw - win + i + 1:stride, j:endh - win + j + 1:stride]
+            Y[:, k, :] = np.array(patch[:]).reshape(endc, TotalPatNum)
+            k = k + 1
+    return Y.reshape([endc, win, win, TotalPatNum])
 
-    .. code-block::
+def gen_data(path, patch_size, stride, mode='train'):
+    """
+    generate train or test dataset, save in h5 file
 
-        - rootdir/
-            - train/
-                - img000.png
-                - img001.png
-            - test/
-                - img000.png
-                - img001.png
+    args:
+        path: root directory of images
+        patch_size: size of image to be crop
+        stride: stride when crop image
+        mode: "train" or "test"
+
+    -path/
+        -train/
+            -img01.png
+            -img01.png
+            ...
+        -valid/
+            -img01.png
+            -img01.png
+            ...
+    """
+    path = os.path.join(path, mode)
+    h5f = h5py.File(path+'/{}.h5'.format(mode), 'w')
+    file_names = glob.glob(os.path.join(path, '*.png'))
+    file_names.sort()
+    toTensor=transforms.ToTensor()# 实例化一个toTensor
+    num = 1
+    for i in range(len(file_names)):
+        print(file_names[i])
+        img = Image.open(file_names[i]).convert("RGB")
+        img = toTensor(img)
+
+        patches = Im2Patch(img, patch_size, stride)
+        patch_num = patches.shape[3]
+        print('{} has {} samples after patch'.format(file_names[i], patch_num))
+
+        
+        for j in range(patch_num):
+            h5f.create_dataset(str(num), data=patches[:,:,:,j], dtype=np.float32)
+            num += 1
+                
+    h5f.close()
+    print('{} {} samples\n'.format(num-1, mode))
+
+class CLIC_dataset(Dataset):
+    """Load database. 
 
     Args:
-        root (string): root directory of the dataset
-        transform (callable, optional): a function or transform that takes in a
-            PIL image and returns a transformed version
-        split (string): split mode ('train' or 'val')
+        root (string): root directory of the h5 file
+        mode: "train" or "valid"
     """
 
-    def __init__(self, root, transform=None, split="train"):
-        splitdir = Path(root) / split
-
-        if not splitdir.is_dir():
-            raise RuntimeError(f'Invalid directory "{root}"')
-
-        self.samples = [f for f in splitdir.iterdir() if f.is_file()]
-
-        self.transform = transform
+    def __init__(self, root, mode="train"):
+        self.mode = mode
+        self.h5f = h5py.File(os.path.join(root, '{}.h5'.format(mode)), 'r')
+        self.keys = list(self.h5f.keys())
+        if self.mode == 'train':
+            random.shuffle(self.keys)
+        else:
+            self.keys.sort()
 
     def __getitem__(self, index):
         """
         Args:
             index (int): Index
-
-        Returns:
-            img: `PIL.Image.Image` or transformed `PIL.Image.Image`.
         """
-        img = Image.open(self.samples[index]).convert("RGB")
-        if self.transform:
-            return self.transform(img)
+        key = self.keys[index]
+        #print(key)
+        data = np.array(self.h5f[key])
+        img = torch.Tensor(data)
+        #print(img.shape)
         return img
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.keys)
+
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+if __name__ == "__main__":
+    root = '/data1/zhaoshuyi/Datasets/CLIC2020/'
+    gen_data(root,256,256,'train')
+    gen_data(root,256,256, 'valid')
+    '''
+    train_transforms = transforms.Compose(
+        [transforms.RandomCrop(256), transforms.ToTensor()]
+    )
+    train_dataset = ImageFolder(root=root, transform=train_transforms, split="train")
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=4,
+        num_workers=1,
+        shuffle=True,
+    )
+    for batch, (inputs) in enumerate(train_dataloader):
+        inputs = inputs.cuda()
+        print(inputs.shape)'''
--- baseline/main.py
+++ baseline/main.py
@@ -1,7 +1,6 @@
 from ast import arg
+from asyncore import write
 from cgi import test
-from curses import savetty
-import imp
 import os
 import torch
 import torch.nn as nn
@@ -11,56 +10,63 @@ import torchvision
 
 from torch import optim
 from torchvision import transforms
+from torch.utils.data import DataLoader
 from torch.autograd import Variable
-from torchsummary import summary
 import matplotlib.pyplot as plt
 from PIL import Image
 from models.model import ContextHyperprior
 from ratedistortionloss import RateDistortionLoss
-from utils import concat_images, AverageMeter
-from dataset import ImageFolder
+from utils import AverageMeter
+from dataset import CLIC_dataset
 
 
-def train_epoch(model, criterion, optimiser, train_dataloader, epoch, epochs, f):
-	"""
+def train_epoch(args, model, criterion, optimiser, train_dataloader, epoch, epochs,
+                f):
+    """
 	Train model for one epoch
 	"""
-	loss = AverageMeter()
-	mse_loss = AverageMeter()
-	bpp_loss = AverageMeter()
-	model.train()  # Set model to training mode
-	
-	for batch, (inputs, _) in enumerate(train_dataloader):
-		inputs = inputs.cuda()
+    loss = AverageMeter()
+    mse_loss = AverageMeter()
+    bpp_loss = AverageMeter()
+    model.train()  # Set model to training mode
 
-		optimiser.zero_grad()
-		
-		# forward
-		out = model(inputs)
+    for batch, inputs in enumerate(train_dataloader):
+        inputs = Variable(inputs.to(args.device))
 
-		out_criterion = criterion(out, inputs)
-		
-		# backward
-		out_criterion["loss"].backward()
-		optimiser.step()
-		
-		# keep track of loss
-		loss.update(out_criterion["loss"].item(), inputs.size(0))
-		mse_loss.update(out_criterion["mse_loss"].item(), inputs.size(0))
-		bpp_loss.update(out_criterion["bpp_loss"].item(), inputs.size(0))
-		
-		# print out loss and visualise results
-		if batch % 10 == 0:
-			print('Epoch {}/{}:[{}]/[{}]'.format(epoch, epochs, batch, len(train_dataloader)).ljust(25), 'Loss: %.4f'.ljust(7)%(loss.avg), 'mse_loss: %.4f'.ljust(7)%(mse_loss.avg), 'bpp_loss: %.4f'.ljust(7)%(bpp_loss.avg))
-			f.write('Epoch {}/{}:[{}]/[{}]'.format(epoch, epochs, batch, len(train_dataloader)).ljust(25), 'Loss: %.4f'.ljust(7)%(loss.avg), 'mse_loss: %.4f'.ljust(7)%(mse_loss.avg), 'bpp_loss: %.4f\n'.ljust(7)%(bpp_loss.avg))
-			'''reconstructed_image = torchvision.transforms.ToPILImage(mode='RGB')(x_hat[0].to('cpu'))
-			image = torchvision.transforms.ToPILImage(mode='RGB')(inputs[0].to('cpu'))
-			result_image = concat_images(image, reconstructed_image)
-			result_image.save("train_images/epoch{}batch{}.png".format(epoch, batch))'''
+        optimiser.zero_grad()
 
-	return losses.avg
+        # forward
+        out = model(inputs)
 
-def test_epoch(model, criterion, test_dataloader, epoch, f):
+        out_criterion = criterion(out, inputs)
+
+        # backward
+        out_criterion["loss"].backward()
+        optimiser.step()
+
+        # keep track of loss
+        loss.update(out_criterion["loss"].item(), inputs.size(0))
+        mse_loss.update(out_criterion["mse_loss"].item(), inputs.size(0))
+        bpp_loss.update(out_criterion["bpp_loss"].item(), inputs.size(0))
+
+        # print out loss and visualise results
+        if batch % 10 == 0:
+            print(
+                'Epoch {}/{}:[{}]/[{}]'.format(
+                    epoch, epochs, batch, len(train_dataloader)).ljust(30),
+                'Loss: %.4f'.ljust(10) % (loss.avg),
+                'mse_loss: %.4f'.ljust(10) % (mse_loss.avg),
+                'bpp_loss: %.4f'.ljust(10) % (bpp_loss.avg))
+            f.write('Epoch {}/{}:[{}]/[{}]'.format(
+                epoch, epochs, batch, len(train_dataloader)).ljust(30))
+            f.write('Loss: %.4f'.ljust(10) % (loss.avg))
+            f.write('mse_loss: %.4f'.ljust(10) % (mse_loss.avg))
+            f.write('bpp_loss: %.4f\n'.ljust(10) % (bpp_loss.avg))
+
+    return loss.avg
+
+
+def test_epoch(args, model, criterion, test_dataloader, epoch, f):
     model.eval()
 
     loss = AverageMeter()
@@ -70,7 +76,7 @@ def test_epoch(model, criterion, test_dataloader, epoch, f):
 
     with torch.no_grad():
         for inputs in test_dataloader:
-            inputs = inputs.cuda()
+            inputs = Variable(inputs.to(args.device))
             out = model(inputs)
             out_criterion = criterion(out, inputs)
 
@@ -78,9 +84,18 @@ def test_epoch(model, criterion, test_dataloader, epoch, f):
             loss.update(out_criterion["loss"])
             mse_loss.update(out_criterion["mse_loss"])
 
-    print('Epoch {}'.format(epoch).ljust(7), 'Loss: %.4f'.ljust(7)%(loss.avg), 'mse_loss: %.4f'.ljust(7)%(mse_loss.avg), 'bpp_loss: %.4f'.ljust(7)%(bpp_loss.avg))
-    f.write('Epoch {}'.format(epoch).ljust(7), 'Loss: %.4f'.ljust(7)%(loss.avg), 'mse_loss: %.4f'.ljust(7)%(mse_loss.avg), 'bpp_loss: %.4f\r'.ljust(7)%(bpp_loss.avg))
+        f.write('Epoch {}'.format(epoch).ljust(15))
+        f.write('Loss: %.4f'.ljust(10) % (loss.avg))
+        f.write('mse_loss: %.4f'.ljust(10) % (mse_loss.avg))
+        f.write('bpp_loss: %.4f\n'.ljust(10) % (bpp_loss.avg))
+
+    print('Epoch {}'.format(epoch).ljust(15),
+          'Loss: %.4f'.ljust(10) % (loss.avg),
+          'mse_loss: %.4f'.ljust(10) % (mse_loss.avg),
+          'bpp_loss: %.4f'.ljust(10) % (bpp_loss.avg))
+
     return loss.avg
+
 
 def plot(y1, y2, label, outf):
     x = np.arange(0, len(y1), 1)
@@ -90,102 +105,149 @@ def plot(y1, y2, label, outf):
     plt.xlabel('epoch')
     plt.ylabel(label)
 
-    plt.savefig(outf+label+'.jpg')
+    plt.savefig(outf + label + '.jpg')
+
 
 def train(args):
-	gpu_num = len(args.gpus.split(','))
-	device_ids = list(range(gpu_num))
+    gpu_num = len(args.gpus.split(','))
+    device_ids = list(range(gpu_num))
 
-	save_path = './results/lr{args.lr}_bs{args.batch_size}_lambda{args.lmbda}/'
-	if not os.path.exists(save_path):
-		os.mkdir(save_path)
+    save_path = './results/lr{}_bs{}_lambda{}/'.format(args.lr, args.batch_size, args.lmbda)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
 
-	train_transforms = transforms.Compose(
-        [transforms.RandomCrop(args.patch_size), transforms.ToTensor()]
-    )
-	test_transforms = transforms.Compose(
-        [transforms.CenterCrop(args.patch_size), transforms.ToTensor()]
-    )
+    train_transforms = transforms.Compose(
+        [transforms.RandomCrop(args.patch_size),
+         transforms.ToTensor()])
+    test_transforms = transforms.Compose(
+        [transforms.CenterCrop(args.patch_size),
+         transforms.ToTensor()])
 
-	# load dataset
-	train_dataset = ImageFolder(args.dataset, split="train", transform=train_transforms)
-	test_dataset = ImageFolder(args.dataset, split="test", transform=test_transforms)
-	
-	# create data loader
-	train_dataloader = DataLoader(
+    # load dataset
+    train_dataset = CLIC_dataset(args.dataset,
+                                mode="train")
+    test_dataset = CLIC_dataset(args.dataset,
+                               mode="valid")
+
+    # create data loader
+    train_dataloader = DataLoader(
         train_dataset,
-        batch_size=args.batch_size,
+        batch_size=args.batch_size*gpu_num,
         num_workers=args.num_workers,
         shuffle=True,
     )
-	test_dataloader = DataLoader(
+    test_dataloader = DataLoader(
         test_dataset,
-        batch_size=args.test_batch_size,
+        batch_size=args.test_batch_size*gpu_num,
         num_workers=args.num_workers,
         shuffle=False,
     )
-	
-	model = ContextHyperprior()
+
+    model = ContextHyperprior()
+
+    criterion = RateDistortionLoss(args.lmbda)
+    criterion.cuda()
+    optimiser = optim.Adam(model.parameters(), lr=args.lr)
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, "min")
+
+    # load model and continue training
+    if args.continue_training:
+        checkpoint = torch.load(args.checkpoint)
+        model.load_state_dict(checkpoint['state_dict'])
+        optimiser.load_state_dict(checkpoint['optimiser'])
+        lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+        start_epoch = checkpoint['epoch']
+        f = open(save_path + 'loss.txt', 'a+')
+    else:
+        start_epoch = 0
+        f = open(save_path + 'loss.txt', 'w+')
+
+    # move model to gpu and show structure
+    if gpu_num > 1:
+        model = nn.DataParallel(model,
+                                device_ids=device_ids,
+                                output_device=device_ids[0])
+    model.to(args.device)
     
-	criterion = RateDistortionLoss(args.lmbda)
-	criterion.cuda()
-	optimiser = optim.Adam(model.parameters(), lr=args.lr)
-	lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimiser, "min")
 
-	# load model and continue training
-	if args.continue_training:
-		checkpoint = torch.load(args.checkpoint)
-		model.load_state_dict(checkpoint['state_dict'])
-		optimiser.load_state_dict(checkpoint['optimiser'])
-		lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
-		start_epoch = checkpoint['epoch']
-		f = open(save_path + 'loss.txt', 'a+')
-	else:
-		start_epoch = 0
-		f = open(save_path + 'loss.txt', 'w+')
-	
-	# move model to gpu and show structure
-	if gpu_num > 1:
-		model = nn.DataParallel(model, device_ids=device_ids, output_device=device_ids[0])
-	model.cuda()
-	summary(model, input_size=train_data[0][0].shape)
+    # training
+    train_loss_sum = []
+    test_loss_sum = []
+    for epoch in range(start_epoch, args.epochs):
+        train_loss = train_epoch(args, model, criterion, optimiser, train_dataloader,
+                                 epoch, args.epochs, f)
+        test_loss = test_epoch(args, model, criterion, test_dataloader, epoch, f)
 
-	# training
-	train_loss_sum = []
-	test_loss_sum = []
-	for epoch in range(start_epoch, args.epochs):
-		train_loss = train_epoch(model, criterion, optimiser, train_dataloader, epoch, args.epochs, f)
-		test_loss = test_epoch(model, criterion, test_dataloader, epoch, f)
+        train_loss_sum.append(train_loss)
+        test_loss_sum.append(test_loss)
 
-		train_loss_sum.append(train_loss)
-		test_loss_sum.append(test_loss)
+        # save the model
+        if epoch % 10 == 9:
+            state = {
+                'epoch': epoch,
+                'state_dict': model.module.state_dict() if gpu_num>1 else model.state_dict(),
+                'optimizer': optimiser.state_dict(),
+                "lr_scheduler": lr_scheduler.state_dict(),
+            }
+            torch.save(
+                state,
+                os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))
 
-		# save the model
-		state = {
-					'epoch': epoch,
-					'state_dict': model.state_dict(),
-					'optimizer': optimiser.state_dict(),
-					"lr_scheduler": lr_scheduler.state_dict(),
-				}
-		torch.save(state, args.checkpoint)
-
-	plot(train_loss_sum, test_loss_sum, 'loss', save_path)
+    plot(train_loss_sum, test_loss_sum, 'loss', save_path)
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('-epochs', type=int, default=1000, help='number of epoch for training')
-	parser.add_argument('-batch_size', type=int, default=4, help='number of epoch for training')
-	parser.add_argument('-continue_training', type=bool, default=False, help='whether to use pretrained model from the checkpoint file')
-	parser.add_argument('-checkpoint', type=str, default='compression_model.pth', help='path where to save checkpoint during training')
-	parser.add_argument('-root', type=str, default='data/', help='path to the folder with images')
-	parser.add_argument('-lr', type=float, default=1e-4, help='path to the folder with grayscale images')
-	parser.add_argument("--gpus", type=str, default="0", help='path log files')
-	parser.add_argument("--lmbda", type=float, default=1e-2, help="Bit-rate distortion parameter (default: %(default)s)",)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-epochs',
+                        type=int,
+                        default=2000,
+                        help='number of epoch for training')
+    parser.add_argument('-batch_size',
+                        type=int,
+                        default=8,
+                        help='number of batch_size for training')
+    parser.add_argument('-test_batch_size',
+                        type=int,
+                        default=4,
+                        help='number of batch_size for testing')
+    parser.add_argument("-n",
+                        "--num-workers",
+                        type=int,
+                        default=1,
+                        help="Dataloaders threads (default: %(default)s)")
+    parser.add_argument('-continue_training',
+						type=bool,
+        				default=False,
+        				help='whether to use pretrained model from the checkpoint file')
+    parser.add_argument('-checkpoint',
+                        type=str,
+                        default='compression_model.pth',
+                        help='path where to save checkpoint during training')
+    parser.add_argument('-dataset',
+                        type=str,
+                        default='/data1/zhaoshuyi/Datasets/CLIC2020/',
+                        help='path to the folder with images')
+    parser.add_argument("--patch-size",
+						type=int,
+						nargs=2,
+						default=(256, 256),
+						help="Size of the patches to be cropped (default: %(default)s)")
+    parser.add_argument('-lr',
+                        type=float,
+                        default=1e-4,
+                        help='path to the folder with grayscale images')
+    parser.add_argument("--gpus", type=str, default="0", help='path log files')
+    parser.add_argument("--device", default="cpu")
+    parser.add_argument("--lmbda",
+						type=float,
+						default=1e-2,
+						help="Bit-rate distortion parameter (default: %(default)s)")
+    args = parser.parse_args()
 
-	os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-	os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+    USE_CUDA = torch.cuda.is_available()
+    args.device = torch.device("cuda:0" if USE_CUDA else "cpu")
 
-	train(args)
-	print("Done.")
+    train(args)
+    print("Done.")
