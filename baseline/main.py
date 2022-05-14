25ccdf16502c69f529ebdc0020424a9ba6ce5db7
Jossieee <1337139691@qq.com>
2022-05-14 20:36:29 +0800

    add aux_loss

--- baseline/main.py
+++ baseline/main.py
@@ -1,6 +1,3 @@
-from ast import arg
-from asyncore import write
-from cgi import test
 import os
 import torch
 import torch.nn as nn
@@ -17,18 +14,51 @@ from PIL import Image
 from models.model import ContextHyperprior
 from ratedistortionloss import RateDistortionLoss
 from utils import AverageMeter
-from dataset import h5dataset
+from dataset import h5dataset, h5dataset_train, ImageFolder
 
 
-def train_epoch(args, model, criterion, optimizer, train_dataloader, epoch, epochs, f):
+def configure_optimizers(net, args):
+    """Separate parameters for the main optimizer and the auxiliary optimizer.
+    Return two optimizers"""
+
+    parameters = {
+        n
+        for n, p in net.named_parameters()
+        if not n.endswith(".quantiles") and p.requires_grad
+    }
+    aux_parameters = {
+        n
+        for n, p in net.named_parameters()
+        if n.endswith(".quantiles") and p.requires_grad
+    }
+
+    # Make sure we don't have an intersection of parameters
+    params_dict = dict(net.named_parameters())
+    inter_params = parameters & aux_parameters
+    union_params = parameters | aux_parameters
+
+    assert len(inter_params) == 0
+    assert len(union_params) - len(params_dict.keys()) == 0
+
+    optimizer = optim.Adam(
+        (params_dict[n] for n in sorted(parameters)),
+        lr=args.lr,
+    )
+    aux_optimizer = optim.Adam(
+        (params_dict[n] for n in sorted(aux_parameters)),
+        lr=args.aux_lr,
+    )
+    return optimizer, aux_optimizer
+
+
+def train_epoch(args, model, criterion, optimizer, aux_optimizer, train_dataloader, epoch,
+                epochs, f):
     """
-	Train model for one epoch
-	"""
-    loss = AverageMeter()
-    mse_loss = AverageMeter()
-    bpp_loss = AverageMeter()
+        Train model for one epoch
+        """
     model.train()  # Set model to training mode
 
+    loss = AverageMeter()
     for batch, inputs in enumerate(train_dataloader):
         inputs = Variable(inputs.to(args.device))
 
@@ -41,30 +71,30 @@ def train_epoch(args, model, criterion, optimizer, train_dataloader, epoch, epoc
 
         # backward
         out_criterion["loss"].backward()
+        nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
 
-        # keep track of loss
-        loss.update(out_criterion["loss"].item(), inputs.size(0))
-        mse_loss.update(out_criterion["mse_loss"].item(), inputs.size(0))
-        bpp_loss.update(out_criterion["bpp_loss"].item(), inputs.size(0))
+        aux_loss = model.aux_loss()
+        aux_loss.backward()
+        aux_optimizer.step()
 
-        if loss.avg > 10 and epoch > 1:
-            for name, parms in model.named_parameters():
-                print('-->name:', name, '-->grad_requirs:',
-                      parms.requires_grad, ' -->grad_value:', parms.grad)
+        loss.update(out_criterion["loss"])
 
         # print out loss and visualise results
         if batch % 10 == 0:
             print(
                 'Epoch {}/{}:[{}]/[{}]'.format(
                     epoch, epochs, batch, len(train_dataloader)).ljust(30),
-                'Loss: %.4f'.ljust(14) % (loss.avg),
-                'mse_loss: %.6f'.ljust(19) % (mse_loss.avg),
-                'bpp_loss: %.4f'.ljust(17) % (bpp_loss.avg))
-            f.write('Epoch {}/{}:[{}]/[{}]'.format(epoch, epochs, batch, len(train_dataloader)).ljust(30))
-            f.write('Loss: %.4f'.ljust(14) % (loss.avg))
-            f.write('mse_loss: %.6f'.ljust(19) % (mse_loss.avg))
-            f.write('bpp_loss: %.4f\n'.ljust(17) % (bpp_loss.avg))
+                'Loss: %.4f'.ljust(14) % (out_criterion["loss"]),
+                'mse_loss: %.6f'.ljust(19) % (out_criterion["mse_loss"]),
+                'bpp_loss: %.4f'.ljust(17) % (out_criterion["bpp_loss"]))
+            f.write('Epoch {}/{}:[{}]/[{}]'.format(
+                epoch, epochs, batch, len(train_dataloader)).ljust(30))
+            f.write('Loss: %.4f'.ljust(14) % (out_criterion["loss"]))
+            f.write('mse_loss: %.6f'.ljust(19) %
+                    (out_criterion["mse_loss"]))
+            f.write('bpp_loss: %.4f\n'.ljust(17) %
+                    (out_criterion["bpp_loss"]))
 
     return loss.avg
 
@@ -116,14 +146,29 @@ def train(args):
     gpu_num = len(args.gpus.split(','))
     device_ids = list(range(gpu_num))
 
-    save_path = './results/{}_chN{}_chM{}_lambda{}_bs{}_lr{}_miles{}_gamma{}/'.format(args.train_data,
-        args.channel_N, args.channel_M, args.lmbda, args.batch_size, args.lr, args.milestones, args.gamma)
+    save_path = './results/{}_chN{}_chM{}_lambda{}_bs{}_lr{}_miles{}_gamma{}/'.format(
+        args.train_data, args.channel_N, args.channel_M, args.lmbda,
+        args.batch_size * gpu_num, args.lr, args.milestones, args.gamma)
     if not os.path.exists(save_path):
         os.mkdir(save_path)
 
     # load dataset
-    train_dataset = h5dataset(mode="train", h5path='./data/train_{}.h5'.format(args.train_data))
-    test_dataset = h5dataset( mode="valid", h5path='./data/valid.h5')
+    train_transforms = transforms.Compose(
+        [transforms.RandomCrop(args.patch_size),
+         transforms.ToTensor()])
+
+    test_transforms = transforms.Compose(
+        [transforms.CenterCrop(args.patch_size),
+         transforms.ToTensor()])
+    train_dataset = ImageFolder(args.dataset,
+                                split="train",
+                                transform=train_transforms)
+    test_dataset = ImageFolder(args.dataset,
+                               split="test",
+                               transform=test_transforms)
+
+    #train_dataset = h5dataset(mode="train", h5path='./data/train_{}.h5'.format(args.train_data))
+    #test_dataset = h5dataset( mode="valid", h5path='./data/valid_CLIC.h5')
 
     # create data loader
     train_dataloader = DataLoader(
@@ -144,18 +189,19 @@ def train(args):
 
     criterion = RateDistortionLoss(args.lmbda)
     criterion.cuda()
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    #lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
+
+    optimizer, aux_optimizer = configure_optimizers(model, args)
+
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
     #lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.8)
-    lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer,
-                                                  args.milestones,
-                                                  gamma=args.gamma)
+    #lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, args.milestones, gamma=args.gamma)
 
     # load model and continue training
     if args.continue_training:
         checkpoint = torch.load(args.checkpoint)
         model.load_state_dict(checkpoint['state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
+        aux_optimizer.load_state_dict(checkpoint["aux_optimizer"])
         lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
         start_epoch = checkpoint['epoch']
         f = open(save_path + 'loss.txt', 'a+')
@@ -174,18 +220,19 @@ def train(args):
     train_loss_sum = []
     test_loss_sum = []
     for epoch in range(start_epoch, args.epochs):
-        train_loss = train_epoch(args, model, criterion, optimizer,
+
+        print("第%d个epoch的学习率：%f" % (epoch, optimizer.param_groups[0]['lr']))
+        train_loss = train_epoch(args, model, criterion, optimizer, aux_optimizer,
                                  train_dataloader, epoch, args.epochs, f)
         test_loss = test_epoch(args, model, criterion, test_dataloader, epoch,
                                f)
-        lr_scheduler.step()
-        print("第%d个epoch的学习率：%f" % (epoch, optimizer.param_groups[0]['lr']))
+        lr_scheduler.step(test_loss)
 
         train_loss_sum.append(train_loss)
         test_loss_sum.append(test_loss)
 
         # save the model
-        if epoch % 100 == 9:
+        '''if epoch % 20 == 19:
             state = {
                 'epoch': epoch,
                 'state_dict': model.module.state_dict() if gpu_num > 1 else model.state_dict(),
@@ -194,22 +241,41 @@ def train(args):
             }
             torch.save(
                 state,
-                os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))
+                os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))'''
+        state = {
+            'epoch': epoch,
+            'state_dict': model.module.state_dict() if gpu_num > 1 else model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            "aux_optimizer": aux_optimizer.state_dict(),
+            "lr_scheduler": lr_scheduler.state_dict(),
+        }
+        torch.save(
+            state,
+            os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))
         plot(train_loss_sum, test_loss_sum, 'loss', save_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--train_data', type=str, choices=['ImageNet', 'CLIC'], default='ImageNet', help='data for training')
-    parser.add_argument('-epochs',
+    parser.add_argument('--train_data',
+                        type=str,
+                        choices=['ImageNet', 'CLIC'],
+                        default='CLIC',
+                        help='data for training')
+    parser.add_argument("-d",
+                        "--dataset",
+                        type=str,
+                        required=True,
+                        help="Training dataset")
+    parser.add_argument('--epochs',
                         type=int,
-                        default=2000,
+                        default=1000,
                         help='number of epoch for training')
-    parser.add_argument('-batch_size',
+    parser.add_argument('--batch_size',
                         type=int,
                         default=32,
                         help='number of batch_size for training')
-    parser.add_argument('-test_batch_size',
+    parser.add_argument('--test_batch_size',
                         type=int,
                         default=4,
                         help='number of batch_size for testing')
@@ -218,11 +284,11 @@ if __name__ == "__main__":
                         type=int,
                         default=1,
                         help="Dataloaders threads (default: %(default)s)")
-    parser.add_argument('-continue_training',
+    parser.add_argument('--continue_training',
                         type=bool,
                         default=False,
                         help='whether to use pretrained model from the checkpoint file')
-    parser.add_argument('-checkpoint',
+    parser.add_argument('--checkpoint',
                         type=str,
                         default='compression_model.pth',
                         help='path where to save checkpoint during training')
@@ -235,9 +301,12 @@ if __name__ == "__main__":
                         type=float,
                         default=1e-4,
                         help='path to the folder with grayscale images')
+    parser.add_argument("--aux_lr", 
+                        default=1e-3, 
+                        help="Auxiliary loss learning rate (default: %(default)s)",)
     parser.add_argument("--milestones",
                         type=list,
-                        default=[5],
+                        default=[10],
                         help="how many epoch to reduce the lr")
     parser.add_argument("--gamma",
                         type=int,
@@ -257,6 +326,7 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
     USE_CUDA = torch.cuda.is_available()
     args.device = torch.device("cuda:0" if USE_CUDA else "cpu")
+    print(args)
 
     train(args)
     print("Done.")
