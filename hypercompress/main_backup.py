7613c72391b2f3110b644b4ea1d4c97483b76a56
Jossieee <1337139691@qq.com>
2022-05-27 11:31:02 +0800

    new

---
+++ hypercompress/main_backup.py
@@ -0,0 +1,377 @@
+from email.policy import default
+from builtins import type
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+import argparse
+import torchvision
+import matplotlib.pyplot as plt
+
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+
+from ratedistortionloss import  RateDistortionLoss, RateDistortion_SAM_Loss
+from utils import AverageMeter
+
+from dataset_hsi import CAVE_Dataset
+
+from models.ContextHyperprior import ContextHyperprior
+from models.cheng2020attention import Cheng2020Attention
+from models.hypercompress import HyperCompress1
+
+
+
+def configure_optimizers(net, args):
+    """Separate parameters for the main optimizer and the auxiliary optimizer.
+    Return two optimizers"""
+
+    parameters = {
+        n
+        for n, p in net.named_parameters()
+        if not n.endswith(".quantiles") and p.requires_grad
+    }
+    aux_parameters = {
+        n
+        for n, p in net.named_parameters()
+        if n.endswith(".quantiles") and p.requires_grad
+    }
+
+    # Make sure we don't have an intersection of parameters
+    params_dict = dict(net.named_parameters())
+    inter_params = parameters & aux_parameters
+    union_params = parameters | aux_parameters
+
+    assert len(inter_params) == 0
+    assert len(union_params) - len(params_dict.keys()) == 0
+
+    optimizer = optim.Adam(
+        (params_dict[n] for n in sorted(parameters)),
+        lr=args.lr,
+    )
+    aux_optimizer = optim.Adam(
+        (params_dict[n] for n in sorted(aux_parameters)),
+        lr=args.aux_lr,
+    )
+    return optimizer, aux_optimizer
+
+
+def train_epoch(args, model, criterion, optimizer, aux_optimizer,
+                train_dataloader, epoch, epochs, f):
+    """
+        Train model for one epoch
+        """
+    model.train()  # Set model to training mode
+
+    loss = AverageMeter()
+    for batch, inputs in enumerate(train_dataloader):
+        inputs = Variable(inputs.to(args.device))
+
+        optimizer.zero_grad()
+
+        # forward
+        out = model(inputs)
+
+        out_criterion = criterion(out, inputs)
+
+        # backward
+        out_criterion["loss"].backward()
+        nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+
+        aux_loss = model.aux_loss()
+        aux_loss.backward()
+        aux_optimizer.step()
+
+        loss.update(out_criterion["loss"])
+
+        # print out loss and visualise results
+        if batch % 10 == 0:
+            print(
+                'Epoch {}/{}:[{}]/[{}]'.format(
+                    epoch, epochs, batch, len(train_dataloader)).ljust(30),
+                'Loss: %.4f'.ljust(14) % (out_criterion["loss"]),
+                'mse_loss: %.6f'.ljust(19) % (out_criterion["mse_loss"]),
+                'bpp_loss: %.4f'.ljust(17) % (out_criterion["bpp_loss"]),
+                'aux_loss: %.2f'.ljust(17) % (aux_loss.item()))
+            f.write('Epoch: {}/{}:[{}]/[{}]'.format(
+                epoch, epochs, batch, len(train_dataloader)).ljust(30))
+            f.write('Loss: %.4f'.ljust(14) % (out_criterion["loss"]))
+            f.write('mse_loss: %.6f'.ljust(19) % (out_criterion["mse_loss"]))
+            f.write('bpp_loss: %.4f'.ljust(17) % (out_criterion["bpp_loss"]))
+            f.write('aux_loss: %.2f\n'.ljust(17) % (aux_loss.item()))
+
+    return loss.avg
+
+
+def test_epoch(args, model, criterion, test_dataloader, epoch, f):
+    model.eval()
+
+    loss = AverageMeter()
+    bpp_loss = AverageMeter()
+    mse_loss = AverageMeter()
+    with torch.no_grad():
+        for inputs in test_dataloader:
+            inputs = Variable(inputs.to(args.device))
+            out = model(inputs)
+            out_criterion = criterion(out, inputs)
+            bpp_loss.update(out_criterion["bpp_loss"])
+            mse_loss.update(out_criterion["mse_loss"])
+            loss.update(out_criterion["loss"])
+            
+    
+
+    f.write('Epoch {} valid '.format(epoch).ljust(30))
+    f.write('Loss: %.4f'.ljust(14) % (loss.avg))
+    f.write('mse_loss: %.6f'.ljust(19) % (mse_loss.avg))
+    f.write('bpp_loss: %.4f\n'.ljust(17) % (bpp_loss.avg))
+
+    print('Epoch {} valid '.format(epoch).ljust(30),
+          'Loss: %.4f'.ljust(14) % (loss.avg),
+          'mse_loss: %.6f'.ljust(19) % (mse_loss.avg),
+          'bpp_loss: %.4f'.ljust(17) % (bpp_loss.avg))
+
+    return loss.avg
+
+
+def plot(y1, y2, label, outf):
+    x = np.arange(0, len(y1), 1)
+    plt.plot(x, y1, label='train')
+    plt.plot(x, y2, label='valid')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel(label)
+
+    plt.savefig(outf + label + '.jpg')
+    plt.cla()
+    plt.close("all")
+
+
+def train(args):
+    gpu_num = len(args.gpus.split(','))
+    device_ids = list(range(gpu_num))
+
+    save_path = './results/{}_{}_chN{}_chM{}_lambda{}_bs{}_lr{}/'.format(
+        args.model, args.train_data, args.channel_N, args.channel_M,
+        args.lmbda, args.batch_size * gpu_num, args.lr)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+
+    # load dataset
+    if args.train_data == 'CAVE':
+        path = '/data3/zhaoshuyi/Datasets/CAVE/hsi/'
+        bands = 31
+        train_dataset = CAVE_Dataset(path,
+                                     args.patch_size,
+                                     args.stride,
+                                     data_aug = True if args.aug == 'aug' else False, 
+                                     mode='train')
+        valid_dataset = CAVE_Dataset(path, args.patch_size, mode='valid')
+    #train_dataset = h5dataset(mode="train", h5path='./data/train_{}.h5'.format(args.train_data))
+    #test_dataset = h5dataset( mode="valid", h5path='./data/valid_CLIC.h5')
+
+    # create data loader
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size * gpu_num,
+        num_workers=args.num_workers,
+        shuffle=True,
+    )
+    valid_dataloader = DataLoader(
+        valid_dataset,
+        batch_size=args.test_batch_size * gpu_num,
+        num_workers=args.num_workers,
+        shuffle=False,
+    )
+    if args.model == 'mbt':
+        model = ContextHyperprior(channel_in=bands,
+                                  channel_N=args.channel_N,
+                                  channel_M=args.channel_M,
+                                  channel_out=bands)
+    elif args.model == 'cheng2020':
+        model = Cheng2020Attention(channel_in=bands,
+                                   channel_N=args.channel_N,
+                                   channel_M=args.channel_M,
+                                   channel_out=bands)
+    elif args.model == 'HyperCompress1':
+        model = HyperCompress1(channel_in=bands,
+                                channel_N=args.channel_N,
+                                channel_M=args.channel_M,
+                                channel_out=bands)
+    elif args.model == 'HyperCompress2':
+        from models.hypercompress2 import HyperCompress2
+        model = HyperCompress2(channel_in=bands,
+                                channel_N=args.channel_N,
+                                channel_M=args.channel_M,
+                                channel_out=bands)
+    elif args.model == 'HyperCompress3':
+        from models.hypercompress3 import HyperCompress3
+        model = HyperCompress3(channel_in=bands,
+                                channel_N=args.channel_N,
+                                channel_M=args.channel_M,
+                                channel_out=bands)
+    elif args.model == 'HyperCompress4':
+        from models.hypercompress4 import HyperCompress4
+        model = HyperCompress4(channel_in=bands,
+                                channel_N=args.channel_N,
+                                channel_M=args.channel_M,
+                                channel_out=bands)
+
+    criterion = RateDistortionLoss(args.lmbda)
+    #criterion = RateDistortion_SAM_Loss(args.lmbda, args.beta)
+    criterion.cuda()
+
+    optimizer, aux_optimizer = configure_optimizers(model, args)
+
+    lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
+    #lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.8)
+    #lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, args.milestones, gamma=args.gamma)
+
+    # load model and continue training
+    if args.continue_training:
+        checkpoint = torch.load(args.checkpoint)
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        aux_optimizer.load_state_dict(checkpoint["aux_optimizer"])
+        lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+        start_epoch = checkpoint['epoch']
+        f = open(save_path + 'loss.txt', 'a+')
+    else:
+        start_epoch = 0
+        f = open(save_path + 'loss.txt', 'w+')
+
+    # move model to gpu and show structure
+    if gpu_num > 1:
+        model = nn.DataParallel(model,
+                                device_ids=device_ids,
+                                output_device=device_ids[0])
+    model.to(args.device)
+
+    # training
+    train_loss_sum = []
+    valid_loss_sum = []
+    for epoch in range(start_epoch, args.epochs):
+
+        print("第%d个epoch的学习率：%f" % (epoch, optimizer.param_groups[0]['lr']))
+        train_loss = train_epoch(args, model, criterion, optimizer,
+                                 aux_optimizer, train_dataloader, epoch,
+                                 args.epochs, f)
+        valid_loss = test_epoch(args, model, criterion, valid_dataloader,
+                                epoch, f)
+        lr_scheduler.step(valid_loss)
+
+        train_loss_sum.append(train_loss.cpu().detach().numpy())
+        valid_loss_sum.append(valid_loss.cpu().detach().numpy())
+
+        # save the model
+        if epoch % 50 == 49:
+            state = {
+                'epoch': epoch,
+                'state_dict': model.module.state_dict() if gpu_num > 1 else model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                "aux_optimizer": aux_optimizer.state_dict(),
+                "lr_scheduler": lr_scheduler.state_dict(),
+            }
+            torch.save(
+                state,
+                os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))
+        '''state = {
+            'epoch': epoch,
+            'state_dict': model.module.state_dict() if gpu_num > 1 else model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            "aux_optimizer": aux_optimizer.state_dict(),
+            "lr_scheduler": lr_scheduler.state_dict(),
+        }
+        torch.save(
+            state,
+            os.path.join(save_path, "checkpoint_{}.pth".format(epoch + 1)))'''
+        plot(train_loss_sum, valid_loss_sum, 'loss', save_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--train_data',
+                        type=str,
+                        choices=['CAVE'],
+                        default='CAVE',
+                        help='data for training')
+    parser.add_argument('--aug', 
+                        type=str, 
+                        default='aug',
+                        help='whether to augment data.')
+    parser.add_argument("--patch-size",
+                        type=int,
+                        default=256,
+                        help="Size of the patches to be cropped")
+    parser.add_argument("--stride",
+                        type=int,
+                        default=128,
+                        help="Stride when crop paches")
+    parser.add_argument("-n",
+                        "--num-workers",
+                        type=int,
+                        default=4,
+                        help="Dataloaders threads (default: %(default)s)")
+    parser.add_argument('--model',
+                        type=str,
+                        default='cheng2020',
+                        help='Model architecture')
+    parser.add_argument('--channel_N', type=int, default=192)
+    parser.add_argument('--channel_M', type=int, default=192)
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=1000,
+                        help='number of epoch for training')
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=16,
+                        help='number of batch_size for training')
+    parser.add_argument('--test_batch_size',
+                        type=int,
+                        default=4,
+                        help='number of batch_size for testing')
+    parser.add_argument('-lr',
+                        type=float,
+                        default=1e-4,
+                        help='path to the folder with grayscale images')
+    parser.add_argument("--aux_lr",
+                        default=1e-3,
+                        help="Auxiliary loss learning rate.")
+    parser.add_argument("--lmbda",
+                        type=float,
+                        default=1e-2,
+                        help="Bit-rate distortion parameter")
+    parser.add_argument("--beta",
+                        type=float,
+                        default=1e-2,
+                        help="sam loss parameter")
+    parser.add_argument('--continue_training',
+                        type=bool,
+                        default=False,
+                        help='whether to use pretrained model from the checkpoint file')
+    parser.add_argument('--checkpoint',
+                        type=str,
+                        default='compression_model.pth',
+                        help='path where to save checkpoint during training')
+
+    parser.add_argument("--milestones",
+                        type=list,
+                        default=[10],
+                        help="how many epoch to reduce the lr")
+    parser.add_argument("--gamma",
+                        type=int,
+                        default=0.5,
+                        help="how much to reduce the lr each time")
+    parser.add_argument("--gpus", type=str, default="0", help='path log files')
+    parser.add_argument("--device", default="cpu")
+    args = parser.parse_args()
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+    USE_CUDA = torch.cuda.is_available()
+    args.device = torch.device("cuda:0" if USE_CUDA else "cpu")
+    print(args)
+
+    train(args)
+    print("Done.")
---
+++ hypercompress/models/ContextHyperprior.py
@@ -0,0 +1,310 @@
+"""
+Implementation of the model from the paper
+
+Minnen, David, Johannes Ballé, and George D. Toderici.
+["Joint autoregressive and hierarchical priors for learned image compression."](http://papers.nips.cc/paper/8275-joint-autoregressive-and-hierarchical-priors-for-learned-image-compression.pdf
+) Advances in Neural Information Processing Systems. 2018.
+"""
+
+import torch
+from torch import nn
+
+from models.gdn import GDN
+from models.masked_conv import MaskedConv2d
+from models.entropy_models import EntropyBottleneck, GaussianConditional
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, channel_in=3, channel_mid=192, channel_out=192):
+        super(Encoder, self).__init__()
+
+        self.first_conv = nn.Conv2d(in_channels=channel_in,
+                                    out_channels=channel_mid,
+                                    kernel_size=5,
+                                    stride=2,
+                                    padding=5 // 2)
+        self.gdn1 = GDN(channel_mid, inverse=False)
+        self.conv1 = nn.Conv2d(in_channels=channel_mid,
+                               out_channels=channel_mid,
+                               kernel_size=5,
+                               stride=2,
+                               padding=5 // 2)
+        self.gdn2 = GDN(channel_mid, inverse=False)
+        self.conv2 = nn.Conv2d(in_channels=channel_mid,
+                               out_channels=channel_mid,
+                               kernel_size=5,
+                               stride=2,
+                               padding=5 // 2)
+        self.gdn3 = GDN(channel_mid, inverse=False)
+        self.conv3 = nn.Conv2d(in_channels=channel_mid,
+                               out_channels=channel_out,
+                               kernel_size=5,
+                               stride=2,
+                               padding=5 // 2)
+
+    def forward(self, x):
+        x = self.first_conv(x)
+        x = self.gdn1(x)
+        x = self.conv1(x)
+        x = self.gdn2(x)
+        x = self.conv2(x)
+        x = self.gdn3(x)
+        x = self.conv3(x)
+        return x
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, channel_in=192, channel_mid=192, channel_out=3):
+        super(Decoder, self).__init__()
+
+        self.deconv1 = nn.ConvTranspose2d(in_channels=channel_in,
+                                          out_channels=channel_mid,
+                                          kernel_size=5,
+                                          stride=2,
+                                          output_padding=1,
+                                          padding=5 // 2)
+        self.igdn1 = GDN(channel_mid, inverse=True)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=channel_mid,
+                                          out_channels=channel_mid,
+                                          kernel_size=5,
+                                          stride=2,
+                                          output_padding=1,
+                                          padding=5 // 2)
+        self.igdn2 = GDN(channel_mid, inverse=True)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=channel_mid,
+                                          out_channels=channel_mid,
+                                          kernel_size=5,
+                                          stride=2,
+                                          output_padding=1,
+                                          padding=5 // 2)
+        self.igdn3 = GDN(channel_mid, inverse=True)
+        self.last_deconv = nn.ConvTranspose2d(in_channels=channel_mid,
+                                              out_channels=channel_out,
+                                              kernel_size=5,
+                                              stride=2,
+                                              output_padding=1,
+                                              padding=5 // 2)
+        self.igdn = GDN(channel_mid, inverse=True)
+
+    def forward(self, x):
+        x = self.deconv1(x)
+        x = self.igdn1(x)
+        x = self.deconv2(x)
+        x = self.igdn2(x)
+        x = self.deconv3(x)
+        x = self.igdn3(x)
+        x = self.last_deconv(x)
+        return x
+
+
+class HyperEncoder(nn.Module):
+
+    def __init__(self, channel_in=192, channel_out=192):
+        super(HyperEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=channel_in,
+                               out_channels=channel_out,
+                               kernel_size=3,
+                               stride=1,
+                               padding=3 // 2)
+        self.conv2 = nn.Conv2d(in_channels=channel_out,
+                               out_channels=channel_out,
+                               kernel_size=5,
+                               stride=2,
+                               padding=5 // 2)
+        self.conv3 = nn.Conv2d(in_channels=channel_out,
+                               out_channels=channel_out,
+                               kernel_size=5,
+                               stride=2,
+                               padding=5 // 2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.conv2(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.conv3(x)
+        return x
+
+
+class HyperDecoder(nn.Module):
+
+    def __init__(self, channel_in=192, channel_mid=192):
+        super(HyperDecoder, self).__init__()
+
+        self.deconv1 = nn.ConvTranspose2d(in_channels=channel_in,
+                                          out_channels=channel_mid,
+                                          kernel_size=5,
+                                          stride=2,
+                                          output_padding=1,
+                                          padding=5 // 2)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=channel_mid,
+                                          out_channels=channel_mid * 3 // 2,
+                                          kernel_size=5,
+                                          stride=2,
+                                          output_padding=1,
+                                          padding=5 // 2)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=channel_mid * 3 // 2,
+                                          out_channels=channel_mid * 2,
+                                          kernel_size=3,
+                                          stride=1,
+                                          output_padding=0,
+                                          padding=3 // 2)
+
+    def forward(self, x):
+        x = self.deconv1(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.deconv2(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.deconv3(x)
+        return x
+
+
+class EntropyParameters(nn.Module):
+
+    def __init__(self, channel_in):
+        super(EntropyParameters, self).__init__()
+        channel_mid = channel_in // 4
+        self.conv1 = nn.Conv2d(in_channels=channel_in,
+                               out_channels=channel_mid * 10 // 3,
+                               kernel_size=1,
+                               stride=1)
+        self.conv2 = nn.Conv2d(in_channels=channel_mid * 10 // 3,
+                               out_channels=channel_mid * 8 // 3,
+                               kernel_size=1,
+                               stride=1)
+        self.conv3 = nn.Conv2d(in_channels=channel_mid * 8 // 3,
+                               out_channels=channel_mid * 2,
+                               kernel_size=1,
+                               stride=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.conv2(x)
+        x = nn.LeakyReLU(inplace=True)(x)
+        x = self.conv3(x)
+        return x
+
+
+class ContextPrediction(nn.Module):
+
+    def __init__(self, channel_in=192):
+        super(ContextPrediction, self).__init__()
+        self.masked = MaskedConv2d("A",
+                                   in_channels=channel_in,
+                                   out_channels=channel_in * 2,
+                                   kernel_size=5,
+                                   stride=1,
+                                   padding=2)
+
+    def forward(self, x):
+        return self.masked(x)
+
+
+class ContextHyperprior(nn.Module):
+
+    def __init__(self,
+                 channel_in=3,
+                 channel_N=128,
+                 channel_M=192,
+                 channel_out=3):
+        super(ContextHyperprior, self).__init__()
+        self.encoder = Encoder(channel_in=channel_in,
+                               channel_mid=channel_N,
+                               channel_out=channel_M)
+        self.decoder = Decoder(channel_in=channel_M,
+                               channel_mid=channel_N,
+                               channel_out=channel_out)
+
+        self.hyper_encoder = HyperEncoder(channel_in=channel_M,
+                                          channel_out=channel_N)
+        self.hyper_decoder = HyperDecoder(channel_in=channel_N,
+                                          channel_mid=channel_M)
+
+        self.entropy_parameters = EntropyParameters(channel_in=channel_M * 4)
+        self.context = ContextPrediction(channel_in=channel_M)
+        self.entropy_bottleneck = EntropyBottleneck(channels=channel_N)
+        self.gaussian = GaussianConditional(None)
+
+    def aux_loss(self):
+        """Return the aggregated loss over the auxiliary entropy bottleneck
+        module(s).
+        """
+        aux_loss = sum(m.loss() for m in self.modules()
+                       if isinstance(m, EntropyBottleneck))
+        return aux_loss
+
+    def forward(self, x):
+        #print(x.shape)
+        y = self.encoder(x)
+        #print("y:", y.shape)
+        z = self.hyper_encoder(y)
+        #print("z:", z.shape)
+        z_hat, z_likelihoods = self.entropy_bottleneck(z)
+        #print("z_hat:", z_hat.shape)
+        #print("z_lakelihoods:", z_likelihoods.shape)
+        psi = self.hyper_decoder(z_hat)
+        #print("psi:", psi.shape)
+
+        y_hat = self.gaussian.quantize(
+            y, "noise" if self.training else "dequantize")
+        #print("y_hat:", y_hat.shape)
+        phi = self.context(y_hat)
+        #print("phi:", phi.shape)
+
+        gaussian_params = self.entropy_parameters(torch.cat((psi, phi), dim=1))
+        #print('gaussian_params', gaussian_params.shape)
+        scales_hat, means_hat = gaussian_params.chunk(2, 1)
+        #print("scales_hat:", scales_hat.shape, 'means_hat:', means_hat.shape)
+        _, y_likelihoods = self.gaussian(y, scales_hat, means=means_hat)
+        #print("y_likelihoods:", y_likelihoods.shape)
+
+        x_hat = self.decoder(y_hat)
+        #print("x_hat:", x_hat.shape)
+        return {
+            "x_hat": x_hat,
+            "likelihoods": {
+                "y": y_likelihoods,
+                "z": z_likelihoods
+            }
+        }
+
+
+import math
+
+if __name__ == "__main__":
+    net = ContextHyperprior()
+    x = torch.randn(1, 3, 256, 256)
+    out = net(x)
+    x_hat = out['x_hat']
+    likelihoods = out['likelihoods']
+    y_likelihoods = likelihoods['y']
+    z_likelihoods = likelihoods['z']
+    #print(x_hat)
+    #print(y_likelihoods)
+    #print(z_likelihoods)
+    bpp_loss = sum((torch.log(likelihoods).sum() / (-math.log(2) * 256 * 256))
+                   for likelihoods in out["likelihoods"].values())
+    print(bpp_loss)
+    latent_loss = torch.log(y_likelihoods).sum() / (-math.log(2) * 256 * 256)
+    hyper_loss = torch.log(z_likelihoods).sum() / (-math.log(2) * 256 * 256)
+    print(latent_loss + hyper_loss)
+    loss = nn.MSELoss()
+    mse_loss = loss(x_hat, x)
+    print(mse_loss)
+'''
+torch.Size([1, 3, 256, 256])
+y: torch.Size([1, 192, 16, 16])
+z: torch.Size([1, 192, 4, 4])
+z_hat: torch.Size([1, 192, 4, 4])
+z_lakelihoods: torch.Size([1, 192, 4, 4])
+psi: torch.Size([1, 384, 16, 16])
+y_hat: torch.Size([1, 192, 16, 16])
+phi: torch.Size([1, 384, 16, 16])
+gaussian_params torch.Size([1, 384, 16, 16])
+scales_hat: torch.Size([1, 192, 16, 16]) means_hat: torch.Size([1, 192, 16, 16])
+y_likelihoods: torch.Size([1, 192, 16, 16])
+x_hat: torch.Size([1, 3, 256, 256])
+'''
